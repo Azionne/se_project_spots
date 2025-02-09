e77be29b951be0c732c6643c021d75dafb44d768
Azionne <a.vorrice@gmail.com>
2025-02-09 17:24:29 -0500

    Edituserinfo() and addNewCard() added

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -7,7 +7,7 @@ class Api {
   }
 
   getAppInfo() {
-    return Promise.all([this.getInitialCards(), this.getUserInfo]);
+    return Promise.all([this.getInitialCards(), this.getUserInfo()]);
   }
 
   getInitialCards() {
@@ -24,6 +24,39 @@ class Api {
   getUserInfo() {
     return fetch(`${this._baseUrl}/users/me`, {
       headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject("Error: ${res.status}");
+    });
+  }
+
+  editUserInfo({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject("Error: ${res.status}");
+    });
+  }
+
+  addNewCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      //"Content-Type": "application/json; charset=UTF-8",
+      body: JSON.stringify({
+        name,
+        link,
+      }),
     }).then((res) => {
       if (res.ok) {
         return res.json();
